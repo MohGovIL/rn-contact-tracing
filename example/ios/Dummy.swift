6ffd10138a7bd9508eba89a5116be17a931a0bd9
Ran Greenberg <gran33@gmail.com>
2020-04-15 19:36:23 +0300

    add swift config files

---
+++ example/ios/Dummy.swift
@@ -0,0 +1,9 @@
+//
+//  Dummy.swift
+//  example
+//
+//  Created by Ran Greenberg on 15/04/2020.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
---
+++ example/ios/example-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  example-Bridging-Header.h
+//  example
+//
+//  Created by Ran Greenberg on 15/04/2020.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#ifndef example_Bridging_Header_h
+#define example_Bridging_Header_h
+
+
+#endif /* example_Bridging_Header_h */
