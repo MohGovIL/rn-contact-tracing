ebebb9571d4bcc678b50a97341c2b71c10d5b980
Tzufit Lifshitz <tzufit.l@taboola.com>
2020-04-20 15:50:55 +0300

    Add coreData classes

---
+++ lib/ios/db/CoreData/Device+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Device+CoreDataClass.swift
+//  
+//
+//  Created by Tzufit Lifshitz on 4/20/20.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Device)
+public class Device: NSManagedObject {
+
+}
---
+++ lib/ios/db/CoreData/PublicKey+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  PublicKey+CoreDataClass.swift
+//  
+//
+//  Created by Tzufit Lifshitz on 4/20/20.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(PublicKey)
+public class PublicKey: NSManagedObject {
+
+}
