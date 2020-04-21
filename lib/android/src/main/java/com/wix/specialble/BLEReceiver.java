443d2aff286e1ad220821baa7974ed43cdb49f18
Eitan Schwartz <eitan.s@taboola.com>
2020-04-21 14:15:03 +0300

    * We'd need to add publicKey to Config

--- lib/android/src/main/java/com/wix/specialble/BLEReceiver.java
+++ lib/android/src/main/java/com/wix/specialble/BLEReceiver.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import com.wix.specialble.config.Config;
+
 /**
  * Currently filtering in only BOOT_COMPLETE and MY_PACKEGE_REPLACED.
  * NOTE: MY_PACKAGE_REPLACED was added after PACKAGE_REPLACED and is meant to prevent waking the app up every time any app is replaced on device.
@@ -21,7 +23,9 @@ public class BLEReceiver extends BroadcastReceiver {
         //TODO: We might need the intent that starts the service to have intents.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); if we're starting on boot
         //TODO: Make sure other app replacements aren't received here (if so, we'd need to filter using if (intent.getDataString().contains("com.aaa.bbb")){..
         //TODO: Not finding QUICKBOOT_POWERON in Intent.java, this might cause issues
-//        BLEForegroundService.startThisService();
+
+        Config config = Config.getInstance(context);
+        BLEForegroundService.startThisService(context, config.getServiceUUID(), config.getPublicKey());
 
         Log.d(TAG, "Restarting BLE service..");
     }
