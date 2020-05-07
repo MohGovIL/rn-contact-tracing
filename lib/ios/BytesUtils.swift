76df5709b5632af763243d220f18fc905adf6c6a
Yonatan Rimon MacbookPro <yoni.rimon@gmail.com>
2020-05-07 18:10:54 +0300

    BytesUtils

--- lib/ios/BytesUtils.swift
+++ lib/ios/BytesUtils.swift
@@ -1,12 +1,12 @@
 //
 //  BytesUtils.swift
-//  rn-contact-tracing
+//  CryptoSwift
 //
 //  Created by Yonatan Rimon on 07/05/2020.
+//  Copyright © 2020 iGates. All rights reserved.
 //
 
 import Foundation
-
 
 class BytesUtils {
     
@@ -35,5 +35,7 @@ class BytesUtils {
         var pairsMod = pairs
         pairsMod.append(String(string.prefix(2)))
         return toPairsOfChars(pairs: pairsMod, string: String(string.dropFirst(2)))
-    }    
+    }
+
+    
 }
