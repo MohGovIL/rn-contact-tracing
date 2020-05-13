d08b7ff91a1bb780b7a1d47a34c6c61cc81406d9
Yonatan Rimon MacbookPro <yoni.rimon@gmail.com>
2020-05-13 10:47:16 +0300

    CryptoManager empty class added

---
+++ lib/ios/Crypto/CryptoManager.swift
@@ -0,0 +1,13 @@
+//
+//  CryptoManager.swift
+//  CryptoSwift
+//
+//  Created by Yonatan Rimon on 13/05/2020.
+//  Copyright © 2020 iGates. All rights reserved.
+//
+
+import Foundation
+
+class CryptoManager {
+    
+}
